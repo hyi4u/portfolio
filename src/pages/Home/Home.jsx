ccc8ef7bf278ce9e1d4cc30883ab5ea643cf5e77
Haider Ali <haiderumbra@gmail.com>
2026-04-10 13:31:18 +0300

    fix rasied text day 3 try 10

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,12 @@ import HeroSection from "./HeroSection";
 import TopProject from "./TopProjects";
 export default function Home() {
   return (
-    <motion.div className="background-primary">
+    <div className="background-primary">
       <NavBar />
       <div className="max-w-300 min-h-[100dvh] mx-auto  max-xl:max-w-auto max-xl:px-6 max-md:px-6 max-sm:px-4">
         <HeroSection />
         <TopProject />
       </div>
-    </motion.div>
+    </div>
   );
 }
